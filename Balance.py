a34498905bf6f733ed8beae80e8ccd8721802941
NightFore <38656758+NightFore@users.noreply.github.com>
2019-06-26 23:48:11 +0200

    [Game Project 5] Serenity Dawn v1-10b (Alpha Version)

--- Balance.py
+++ Balance.py
@@ -17,6 +17,7 @@ class Player:
         self.level          = level
         self.Experience     = 0
         self.Gold           = 0
+        self.potion         = 2
 
         self.update_level()
 
@@ -25,15 +26,15 @@ class Player:
             self.level += 1
             self.Experience -= 100
         
-        self.Maxhealth      = 45    + (7.90 + 0.525 * (self.level-1)) * (self.level-1)
-        self.Strength       = 8     + (1.25 + 0.225 * (self.level-1)) * (self.level-1)
+        self.maxhealth      = 45    + (5.90 + 0.525 * (self.level-1)) * (self.level-1)
+        self.Strength       = 6     + (1.45 + 0.145 * (self.level-1)) * (self.level-1)
         self.speed          = 9     + (0.95 + 0.100 * (self.level-1)) * (self.level-1)
-        self.Defense        = 4     + (0.75 + 0.190 * (self.level-1)) * (self.level-1)
+        self.Defense        = 4     + (0.75 + 0.165 * (self.level-1)) * (self.level-1)
         
         self.Magic          = 0
         self.Resistance     = 0
 
-        self.Health         = self.Maxhealth
+        self.health         = self.maxhealth
         self.Action_Point   = 0
         
 
@@ -60,15 +61,15 @@ class Iris:
             self.level += 1
             self.Experience -= 100
         
-        self.Maxhealth      = 30    + (6.80 + 0.490 * (self.level-1)) * (self.level-1)
-        self.Strength       = 9     + (0.95 + 0.240 * (self.level-1)) * (self.level-1)
+        self.maxhealth      = 30    + (4.80 + 0.490 * (self.level-1)) * (self.level-1)
+        self.Strength       = 7     + (1.15 + 0.160 * (self.level-1)) * (self.level-1)
         self.speed          = 7     + (0.85 + 0.080 * (self.level-1)) * (self.level-1)
-        self.Defense        = 2     + (0.60 + 0.165 * (self.level-1)) * (self.level-1)
+        self.Defense        = 2     + (0.60 + 0.140 * (self.level-1)) * (self.level-1)
         
         self.Magic          = 0
         self.Resistance     = 0
 
-        self.Health         = self.Maxhealth
+        self.health         = self.maxhealth
         self.Action_Point   = 0
 IrisIG = Iris("Iris")
 
@@ -93,15 +94,15 @@ class Gyrei:
             self.level += 1
             self.Experience -= 100
             
-        self.Maxhealth      = 40    + (8.00 + 0.500 * (self.level-1)) * (self.level-1)
-        self.Strength       = 7     + (1.05 + 0.210 * (self.level-1)) * (self.level-1)
+        self.maxhealth      = 40    + (6.00 + 0.500 * (self.level-1)) * (self.level-1)
+        self.Strength       = 5     + (1.25 + 0.130 * (self.level-1)) * (self.level-1)
         self.speed          = 10    + (1.30 + 0.110 * (self.level-1)) * (self.level-1)
-        self.Defense        = 3     + (0.55 + 0.175 * (self.level-1)) * (self.level-1)
+        self.Defense        = 3     + (0.55 + 0.150 * (self.level-1)) * (self.level-1)
         
         self.Magic          = 0
         self.Resistance     = 0
 
-        self.Health         = self.Maxhealth
+        self.health         = self.maxhealth
         self.Action_Point   = 0
 GyreiIG = Gyrei("Gyrei")
 
@@ -121,15 +122,15 @@ class Wolf:
         self.EXP_Gain       = 10
         self.Gold_Gain      = 10
         
-        self.Maxhealth      = 35    + (5.45 + 0.200 * (self.level-1)) * (self.level-1)
-        self.Strength       = 4     + (1.05 + 0.060 * (self.level-1)) * (self.level-1)
+        self.maxhealth      = 35    + (5.45 + 0.200 * (self.level-1)) * (self.level-1)
+        self.Strength       = 4     + (1.40 + 0.075 * (self.level-1)) * (self.level-1)
         self.speed          = 10    + (1.35 + 0.040 * (self.level-1)) * (self.level-1)
         self.Defense        = 2     + (0.80 + 0.055 * (self.level-1)) * (self.level-1)
         
         self.Magic          = 0
         self.Resistance     = 0
 
-        self.Health         = self.Maxhealth
+        self.health         = self.maxhealth
         self.Action_Point   = 0
 WolfIG = Wolf("Wolf")
 
@@ -145,15 +146,15 @@ class Direwolf:
         self.EXP_Gain       = 10
         self.Gold_Gain      = 10
         
-        self.Maxhealth      = 45    + (7.85 + 0.225 * (self.level-1)) * (self.level-1)
-        self.Strength       = 6     + (1.50 + 0.075 * (self.level-1)) * (self.level-1)
+        self.maxhealth      = 45    + (7.85 + 0.225 * (self.level-1)) * (self.level-1)
+        self.Strength       = 6     + (1.60 + 0.090 * (self.level-1)) * (self.level-1)
         self.speed          = 8     + (1.65 + 0.035 * (self.level-1)) * (self.level-1)
         self.Defense        = 3     + (0.95 + 0.075 * (self.level-1)) * (self.level-1)
         
         self.Magic          = 0
         self.Resistance     = 0
 
-        self.Health         = self.Maxhealth
+        self.health         = self.maxhealth
         self.Action_Point   = 0
 DirewolfIG = Direwolf("Direwolf")
 
@@ -169,15 +170,15 @@ class Shadow_Red:
         self.EXP_Gain       = 10
         self.Gold_Gain      = 10
         
-        self.Maxhealth      = 75    + (7.75 + 0.290 * (self.level-1)) * (self.level-1)
-        self.Strength       = 8     + (0.90 + 0.150 * (self.level-1)) * (self.level-1)
+        self.maxhealth      = 75    + (7.75 + 0.290 * (self.level-1)) * (self.level-1)
+        self.Strength       = 8     + (1.50 + 0.175 * (self.level-1)) * (self.level-1)
         self.speed          = 7     + (1.05 + 0.025 * (self.level-1)) * (self.level-1)
-        self.Defense        = 5     + (0.85 + 0.100 * (self.level-1)) * (self.level-1)
+        self.Defense        = 5     + (0.95 + 0.115 * (self.level-1)) * (self.level-1)
         
         self.Magic          = 0
         self.Resistance     = 0
 
-        self.Health         = self.Maxhealth
+        self.health         = self.maxhealth
         self.Action_Point   = 0
 Shadow_RedIG = Shadow_Red("Shadow_Red")
 
@@ -193,7 +194,7 @@ class Zombie:
         self.EXP_Gain       = 10
         self.Gold_Gain      = 10
 
-        self.Maxhealth      = 72    + 6 * (self.level-1)
+        self.maxhealth      = 72    + 6 * (self.level-1)
         self.Strength       = 14    + 1 * (self.level-1)
         self.speed          = 5     + 1 * (self.level-1)
         self.Defense        = 1     + 1 * (self.level-1)
@@ -201,7 +202,7 @@ class Zombie:
         self.Magic          = 0
         self.Resistance     = 0
 
-        self.Health         = self.Maxhealth
+        self.health         = self.maxhealth
         self.Action_Point   = 0
 ZombieIG = Zombie("Zombie")
 
@@ -217,7 +218,7 @@ class Ghoul:
         self.EXP_Gain       = 10
         self.Gold_Gain      = 10
 
-        self.Maxhealth      = 38    + 6 * (self.level-1)
+        self.maxhealth      = 38    + 6 * (self.level-1)
         self.Strength       = 4     + 1 * (self.level-1)
         self.speed          = 9     + 1 * (self.level-1)
         self.Defense        = 1     + 1 * (self.level-1)
@@ -225,7 +226,7 @@ class Ghoul:
         self.Magic          = 0
         self.Resistance     = 0
 
-        self.Health         = self.Maxhealth
+        self.health         = self.maxhealth
         self.Action_Point   = 0
 GhoulIG = Ghoul("Ghoul")
 
@@ -241,7 +242,7 @@ class Shadow_Blue:
         self.EXP_Gain       = 10
         self.Gold_Gain      = 10
 
-        self.Maxhealth      = 150   + 6 * (self.level-1)
+        self.maxhealth      = 150   + 6 * (self.level-1)
         self.Strength       = 20    + 1 * (self.level-1)
         self.speed          = 13    + 1 * (self.level-1)
         self.Defense        = 1     + 1 * (self.level-1)
@@ -249,7 +250,7 @@ class Shadow_Blue:
         self.Magic          = 0
         self.Resistance     = 0
 
-        self.Health         = self.Maxhealth
+        self.health         = self.maxhealth
         self.Action_Point   = 0
 Shadow_BlueIG = Shadow_Blue("Shadow_Blue")
 
